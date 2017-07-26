1a5fe2e994fce3983deb7dddb6e5ceceb40e2de1
AndrewRPerkins <andrew.perkins@innowell.org>
2017-07-26 10:01:08 +1000

    Adding Test::Unit test generation option

--- lib/generators/interactor.rb
+++ lib/generators/interactor.rb
@@ -1,12 +1,17 @@
 module Interactor
   class Generator < ::Rails::Generators::NamedBase
+    class_option :test_type, type: :string, default: 'spec', aliases: '-t',
+                             desc: 'Should the generated tests be spec or test'
+
     def self.source_root
-      File.expand_path("../templates", __FILE__)
+      File.expand_path('../templates', __FILE__)
     end
 
     def generate
-      template "#{self.class.generator_name}.erb", File.join("app/interactors", class_path, "#{file_name}.rb")
-      template "spec.erb", File.join("spec/interactors", class_path, "#{file_name}_spec.rb")
+      template "#{self.class.generator_name}.erb", File.join('app/interactors', class_path, "#{file_name}.rb")
+      if %w[spec test].include?(options.test_type)
+        template "#{options.test_type}.erb", File.join("#{options.test_type}/interactors", class_path, "#{file_name}_#{options.test_type}.rb")
+      end
     end
   end
 end
